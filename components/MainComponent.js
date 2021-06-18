a0eafc0e16c8c94b40ebd8969a5e0aa3565d897b
ChristianRodarmel <christian.rodarmel1@gmail.com>
2021-06-18 00:39:13 -0700

    Components Part 2/needs recheck as well

---
+++ components/MainComponent.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Directory from './DirectoryComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
+
+import { View } from 'react-native';
+
+import { CAMPSITES } from '../shared/campsites';
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            campsites: CAMPSITES
+        };
+    }
+
+    render() {
+        return <Directory campsites={this.state.campsites} />;
+    }
+}
+
+export default Main;
